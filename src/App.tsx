ecae99152ee5ad8e67bedef5bf40d1f05698786b
michaelvivaldini <60002937+michaelvivaldini@users.noreply.github.com>
2021-04-12 23:56:25 -0300

    styles, formatting, miragejs api

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import { useState } from 'react';
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
+  const [isNewTransactionModalOpen, setNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
       <GlobalStyle />
     </>
   );
